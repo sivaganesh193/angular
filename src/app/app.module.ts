1e4f9aeea5750ba235c405952ab648d5d418b843
ctautomation2020 <70842950+ctautomation2020@users.noreply.github.com>
2020-10-01 12:24:32 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,85 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { PersonDetailsComponent } from './person-details/person-details.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { HomeComponent } from './home/home.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { HomeNewComponent } from './home-new/home-new.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {HttpClientModule} from '@angular/common/http';
+import { PersonComponent } from './person-details/person/person.component';
+import { PersonDetailsRoutingModule } from './person-details/person-details-routing.module';
+import { EducationComponent } from './person-details/education/education.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import { EducationModelComponent } from './person-details/education/education-model/education-model.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PersonDetailsComponent,
+    NavComponent,
+    HomeComponent,
+    HomeNewComponent,
+    PersonComponent,
+    EducationComponent,
+    EducationModelComponent
+  ],
+  entryComponents: [EducationModelComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatGridListModule,
+    MatTabsModule,
+    MatChipsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    LayoutModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    HttpClientModule,
+    PersonDetailsRoutingModule,
+    MatDialogModule,
+    MatDividerModule
+
+  ],
+  providers: [MatDatepickerModule, MatNativeDateModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
